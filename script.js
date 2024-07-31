ee4ea321a0f41f3e788afe3a2e33b4d8ad924619
rociodepoli <rociodepoli69@gmail.com>
2024-07-30 21:11:23 -0300

    palindrome first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+const checkButton= document.getElementById('check-btn');
+const textInput=document.getElementById('text-input');
+const result=document.getElementById('result')
+
+const cleanInputString=(str)=>{
+const regex = /[_\\\/:\-!"$%&=¿¡+()<*>.,?! \s]+/g;
+  return str.replace(regex, '').toLowerCase()
+}
+
+const isPalindrome=(str)=>{
+const arr=str.split('')
+for(let i=0;i<arr.length;i++){
+  let end=arr[arr.length-1]
+  console.log(end, arr[i])
+  if(arr[i]===end){ 
+    arr.pop()
+    
+  } else {
+  return false;}
+}
+return true
+}
+console.log(isPalindrome("notapalindrome"))
+console.log(cleanInputString('0_0 (: /-\ :) 0-0'))
+
+const checkPalindrome= (e)=>{
+e.preventDefault();
+const value= textInput.value
+  if(!value){
+   alert("Please input a value")
+  } else{
+    const cleanInput=cleanInputString(value)
+result.innerHTML=`
+<p><strong>${value}</strong>${isPalindrome(cleanInput)? " is a palindrome":" is not a palindrome"}</p>`
+  }
+}
+
+checkButton.addEventListener('click', checkPalindrome)
